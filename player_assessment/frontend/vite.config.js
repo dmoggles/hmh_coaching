cf196fc68c2e7382fba0227740b042d2cfd54b25
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2026-06-17 00:05:04 +0100

    chore: force new JS filename to bust CDN cache [skip ci]

--- player_assessment/frontend/vite.config.js
+++ player_assessment/frontend/vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/hmh_coaching/player_assessment/dist/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash]-v2.js',
+      }
+    }
+  }
 })
